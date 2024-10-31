96601bb97786c34b1d87c58053f528b21da8ca1f
skyblue1012 <thwn1012@gmail.com>
2024-10-31 11:53:43 +0900

    remove .git files

---
+++ my-app1/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //컴포넌트가 들어오면된다...App.js파일의 App() 함수 부분의 내용이 들어간다.
+  <h1>안녕 리액트~!!</h1>
+);
+
+
